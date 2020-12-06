9dfe95772a824bbcb92ba6bbad017016d5618eb6
Thomas Hennebert <thomas@ineardisplay.com>
2020-12-06 09:59:29 +0100

    Fixed some compiler warnings

--- src/nodes.h
+++ src/nodes.h
@@ -53,35 +53,7 @@ struct TzNodeDoc {
     const char* outputs;
 };
 
-static TzNodeDoc nodesDoc [] = {
-    {"-", "-", "-", "-"},
-    {"module", "special node that processes a patch internally and exposes up to 16 inputs and up to 16 outputs.", "user declared inputs", "user declared outputs"},
-    {"var", "holds a variable that can be shared through the patch. Instead of using myvar@val for I/O, you can simply use $myvar.", "val", "val"},
-    {"add", "outputs {in1} + {in2}.", "in1, in2", "out"},
-    {"sub", "outputs {in1} - {in2}.", "in1, in2", "out"},
-    {"mult", "outputs {in1} * {in2}.", "in1, in2", "out"},
-    {"div", "outputs {in1} / {in2}.", "in1, in2", "out"},
-    {"modulo", "outputs {in1} % {in2}.", "in1, in2", "out"},
-    {"clip", "clips {in} in range [{min}..{max}].", "in, min, max", "out"},
-    {"round", "rounds {in} to the nearest integer value.", "in", "out"},
-    {"and", "outputs 1 if both {in1} and {in2} are not 0, outputs 0 otherwise.", "in1 in2", "out"},
-    {"or", "outputs 1 if either {in1} or {in2} are not 0, outputs 0 otherwise.", "in1 in2", "out"},
-    {"xor", "outputs 1 if one of {in1} and {in2} is not 0, outputs 0 if both are 0 or both are not 0.", "in1 in2", "out"},
-    {"mix", "interpolates between {in1} and {in2} according to {coeff} in range [0..1].", "in1, in2, coeff", "out"},
-    {"map", "maps {in} from the range [{imin}..{imax}] to the range [{omin}..{omax}].", "in, imin, imax, omin, omax", "out"}, 
-    {"miditofreq", "converts a MIDI note [0..127] to a frequency in Hertz.", "in", "out"},
-    {"mem", "1 sample delay.", "in", "out"},
-    {"count", "outputs the count of non zero signals received at {clock}. Loops back to 0 after reaching {max} (inclusive, defaults to 16).", "clock max", "out"},
-    {"phasor", "generates a ramp in the range [0..1]. A pulse at {reset} resets the phase.", "freq(Hz), reset(pulse)", "out"},
-    {"pulse", "outputs a pulse at a periodic rate. A pulse at {reset} resets the phase.", "rate(Ms), reset(pulse)", "out"},
-    {"sinosc", "generates a sine wave. A pulse at {reset} resets the phase. A signal can be sent to {fm) for frequency modulation with the amount of modulation controled by {fmdepth}.", "freq(Hz), reset(pulse), fm, fmdepth", "out"},
-    {"seq8", "outputs the values of inputs {step1} to {step8} sequentially when receiving a pulse at {clock}. The sequence length can be changed via input {length}. The output {pos} sends the playhead position.", "clock(pulse), length(1..8), step1, step2, ..., step8", "out, pos"},
-    {"random", "outputs a random value in the range [0..1] when receiving a pulse at {clock}.", "clock", "out"},
-    {"segment", "outputs a ramp from {val1} to {val2} in {dur} Ms when receiving a pulse at {clock}.", "clock, val1, val2, dur", "out"},
-    {"select", "if {index} is 0, outputs 0 otherwise ouputs the value of the corresponding input.", "index, in1, in2, in3, in4, in5, in6, in7, in8", "out"},
-    {"delay", "a basic delay line (up to 2 seconds).", "in time(Ms)", "out"},
-    {"fdelay", "a delay line with feedback (up to 2 seconds).", "in time(Ms) feed([0..1])", "out"}
-};
+extern const TzNodeDoc nodesDoc [NUM_NODE_TYPES];
 
 typedef struct TzModule TzModule;
 typedef struct TzProcessInfo TzProcessInfo;
